f0d7992ffefe06f52abe8e30c902b80ddca93abb
EricXD756 homer <erichomer29@gmail.com>
2024-09-10 18:07:38 +0000

    added more characters

--- Main.py
+++ Main.py
@@ -4,11 +4,17 @@ import math
 day = 0
 waterCount = 0
 foodCount = 0
+money = 0
 inventory = []
 
-John = character("John", "fine", [], 20, 20, 99, 99)
-John.dialogue = ["Hey there, hows it goin'?", "What's up?", "What's the plan?", "Could I get some water?", "We got any water in the back", "I could go for a cheeseburger right now", "Could we stop for food on the way?", "I feel like I'm 'bouta pass out", "Need.. Water..", "My stomach is turning inside out", "John stares at the wildlife, with his mouth watering", "Aghh, I'm bleeding!", "it hurts! It hurts!", "I don't feel so good", "I'm dying...", "John has passed"]
+char1 = character("John", "fine", [], 20, 20, 99, 99)
+char1.dialogue = ["Hey there, hows it goin'?", "What's up?", "What's the plan?", "Could I get some water?", "We got any water in the back", "I could go for a cheeseburger right now", "Could we stop for food on the way?", "I feel like I'm 'bouta pass out", "Need.. Water..", "My stomach is turning inside out", "John stares at the wildlife, with his mouth watering", "Aghh, I'm bleeding!", "It hurts! it hurts!", "I don't feel so good", "I'm dying...", char1.name + " has passed"]
 
+char2 = character("Beatrice", "fine", [], 20, 20, 99, 99)
+char2.dialogue = ["Hey! How are you?", "Hey there!", "What's next?", "Could I have something to drink?", "I could use some water right now", "Lets go hunting soon", "Could you spare some extra food?", "I feel really light headed", "You think that brown river is safe to drink?", "Please give me food, just anything", "Beatrice munches on her hair in the corner", "Aghh, I't hurts!", "There's blood everywhere!", "I feel really nauseous", "Beatrice is throwing up constantly", char2.name + " has passed"]
+
+char3 = character("Neil", "fine", [], 20, 20, 99, 99)
+char3.dialogue = ["Hi! How are you?", "What's up?", "What's next?", "Man, I'm thirsty", "You got something to drink?", "Lets hunt something, I'm hungry", "You got something to eat?", "The world feels like its spinning", "I feel feint", "That bear over there looks delicious right now", "I haven't eaten for days...", "Aghh, I need something for this cut", "It won't stop bleeding!", "I feel really ill", "Please tell me you brought medicine", char3.name + " has passed"]
 def characterInteract(character, status):
     if status == "fine":
         rand = math.random(0,3)
@@ -61,9 +67,9 @@ def updateStatus(character):
     #Checks the character's stats and updates the status accordingly
     if character.thirst <= 0 or character.hunger <= 0 or character.injuryTimer <= 0 or character.sicknessTimer <= 0:
         character.status = "dead"
-    elif character.thirst < 10 and character.thirst <= character.hunger:
+    elif character.thirst < 12 and character.thirst <= character.hunger:
         character.status = "thirsty"
-    elif character.hunger < 10 and character.hunger < character.thirst:
+    elif character.hunger < 12 and character.hunger < character.thirst:
         character.status = "hungry"
     elif character.thirst < 4 and character.thirst <= character.hunger:
         character.status = "parched"
@@ -72,10 +78,10 @@ def updateStatus(character):
     elif character.injuryTimer > 50 and character.sicknessTimer > 50:
         character.status = "fine"
 
-def passTime(character, hours):
+def passTime(character):
     # Decrease thirst and hunger
-    character.thirst -= 1
-    character.hunger -= 1
+    character.thirst -= 4
+    character.hunger -= 2
 
     # Handle injury and sickness
     if character.injuryTimer < 50:
@@ -101,4 +107,16 @@ def passTime(character, hours):
 
     # Update character status
     character.status = updateStatus(character)
+
+def playGame():
+
+    print("Welcome to the Oregon Trail(Temu Edition)!")
+    print("Your goal is to survive the 30 day trip to oregon")
+    print("You are the leader of your group, and you need to manage your group's resources and make decisions to ensure the survival of your group.")
+    print("Good luck!")
+    
+    if input("Would you like to change your character's name? (Y/N): ").lower() == "y":
+        char1.name = input("Give your first character a name: ")
+        char2.name = input("Give your second character a name: ")
+        char3.name = input("Give your third character a name: ")
 
